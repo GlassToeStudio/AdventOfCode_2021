7e530d3f6ca759a43990a70583598d319385616a
GlassToeStudio <GlassToeStudio@gmail.com>
2021-11-29 20:22:49 -0600

    Update get_input for 2021

--- get_input.py
+++ get_input.py
@@ -1,17 +1,18 @@
+import argparse
 import os
-import sys
 
 import requests
 from dotenv import load_dotenv
 
+from colors import END, RED
+
 
 def import_data(day):
+    load_dotenv()
     ID = os.getenv("SESSION_ID")
-    url = f"https://adventofcode.com/2020/day/{day}/input"
+    url = f"https://adventofcode.com/2021/day/{day}/input"
 
-    cookies = {
-        'session': os.getenv('SESSION_ID'),
-    }
+    cookies = {'session': ID, }
 
     res = requests.get(url, cookies=cookies)
     data = res.content.decode('UTF-8')
@@ -19,24 +20,29 @@ def import_data(day):
 
 
 if __name__ == "__main__":
-    day = sys.argv[1]
-    filename = sys.argv[2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--day", help="Enter the day for which you want the input file.", required=True)  # noqa: E501
+    parser.add_argument("-f", "--file", help="Enter the name to save the input file.", default='input')  # noqa: E501
+    args = parser.parse_args()
 
-    load_dotenv()
-    data = import_data(day)
-
-    if len(day) == 1:
-        day = f"0{day}"
+    data = import_data(args.day)
 
     try:
-        os.mkdir(f"Day_{day}")
+        os.mkdir(f"Day_{args.day}")
     except OSError as error:
+        print(f"{RED}{error}{END}")
         pass
 
-    fi = open(f"Day_{day}/{filename}.txt", 'w')
-    print(data, file=fi)
-    py = open(f"Day_{day}/day_{sys.argv[1]}_problems.py", 'w')
-    s = '"""'
-    upper = "def format_data(data):\n    return [x.strip() for x in data.readlines()]"
-    lower = f"if __name__ == \"__main__\":\n    with open(\"Day_{day}/input.txt\", \"r\") as in_file:\n        data = format_data(in_file)\n        print(data)"
-    print(f"{s}\n{s}\n\n\n{upper}\n\n\n{lower}", file=py)
+    with open(f"Day_{args.day}/{args.file}.txt", 'w') as input_file:
+        print(data, file=input_file)
+
+    with open(f"Day_{args.day}/day_{args.day}_problems.py", 'w') as python_file:  # noqa: E501
+        s = '"""'
+        output = (f"\"\"\"\n\"\"\"\n\n\n"
+                  f"def format_data(data):\n"
+                  f"    return [x.strip() for x in data.readlines()]\n\n\n"
+                  f"if __name__ == \"__main__\":\n"
+                  f"    with open(\"Day_{args.day}/{args.file}.txt\", \"r\") as in_file:\n"  # noqa: E501
+                  f"        data = format_data(in_file)\n"
+                  f"        print(data)")
+        print(output, file=python_file)
