7b97033a3d2f26713a17d75c23c810c9c3c69cdc
GlassToeStudio <GlassToeStudio@gmail.com>
2021-12-02 06:55:18 -0600

    day 2 problems

--- Day_02/day_02_problems.py
+++ Day_02/day_02_problems.py
@@ -39,20 +39,64 @@ and a depth of 10. (Multiplying these together produces 150.)
 Calculate the horizontal position and depth you would have after following the
 planned course. What do you get if you multiply your final horizontal position
 by your final depth?
+
+
+--- Part Two ---
+Based on your calculations, the planned course doesn't seem to make any sense.
+You find the submarine manual and discover that the process is actually
+slightly more complicated.
+
+In addition to horizontal position and depth, you'll also need to track a third
+value, aim, which also starts at 0. The commands also mean something entirely
+different than you first thought:
+
+
+down X increases your aim by X units.
+up X decreases your aim by X units.
+forward X does two things:
+It increases your horizontal position by X units.
+It increases your depth by your aim multiplied by X.
+
+
+Again note that since you're on a submarine, down and up do the opposite of
+what you might expect: "down" means aiming in the positive direction.
+
+Now, the above example does something different:
+
+forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is
+0, your depth does not change.
+
+down 5 adds 5 to your aim, resulting in a value of 5.
+forward 8 adds 8 to your horizontal position, a total of 13. Because your aim
+is 5, your depth increases by 8*5=40.
+
+up 3 decreases your aim by 3, resulting in a value of 2.
+down 8 adds 8 to your aim, resulting in a value of 10.
+forward 2 adds 2 to your horizontal position, a total of 15.  Because your aim
+is 10, your depth increases by 2*10=20 to a total of 60.
+
+
+After following these new instructions, you would have a horizontal position of
+15 and a depth of 60. (Multiplying these produces 900.)
+
+Using this new interpretation of the commands, calculate the horizontal
+position and depth you would have after following the planned course. What do
+you get if you multiply your final horizontal position by your final depth?
+
 """
 
 
 from io import TextIOWrapper
 
 
-def format_data(data: TextIOWrapper) -> list[str]:
+def format_data(data: TextIOWrapper) -> list[list[str, int]]:
     """Return a list of ints from the given text."
 
     Args:
         data (TextIOWrapper): text file
 
     Returns:
-        list[str]: input data as list[str]
+        list[list[str, int]] formatted data
     """
     return [[x.strip()[0], int(x.strip()[-1])] for x in data.readlines()]
 
@@ -61,10 +105,27 @@ def calculate_position(course):
     h = sum(x[1] if x[0] == 'f' else 0 for x in course)
     d = sum(x[1] if x[0] == 'd' else 0 for x in course)
     d -= sum(x[1] if x[0] == 'u' else 0 for x in course)
-    print(h*d)
+    return h * d
+
+
+def calculate_position_with_aim(course):
+    h = d = aim = 0
+    for c in course:
+        if c[0] == 'f':
+            h += c[1]
+            d += aim * c[1]
+        if c[0] == 'd':
+            aim += c[1]
+        if c[0] == 'u':
+            aim -= c[1]
+    return h * d
 
 
 if __name__ == "__main__":
     with open("Day_02/input.txt", "r") as in_file:
         data = format_data(in_file)
-        calculate_position(data)
+        print(f'Part 1: {calculate_position(data)}')
+        print(f'Part 2: {calculate_position_with_aim(data)}')
+
+# Part 1:    2215080
+# Part 2: 1864715580
